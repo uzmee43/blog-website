d691650bc45a0325798bbdc7c52cea272aa7e1cd
uzmee43 <uk0172634@gmail.com>
2025-01-06 20:17:40 +0500

    modified next file

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
+const nextConfig: NextConfig = {
+  /* config options here */
+  eslint:{
+    ignoreDuringBuilds: true,
+    
+  },
+};
 
-// export default nextConfig;
+export default nextConfig;
 module.exports = {
   reactStrictMode: true,
   images: {
